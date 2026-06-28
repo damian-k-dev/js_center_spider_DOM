328c6d7fb818547623195f1794a5af03b5bc7acf
Damian Kurzydlo <duchozywia2024@gmail.com>
2026-06-28 20:20:40 +0100

    task solution fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
 const leftPos = wall.clientWidth / 2 - spider.clientWidth / 2;
-const topPos = wall.clientWidth / 2 - spider.clientWidth / 2;
+const topPos = wall.clientHeight / 2 - spider.clientHeight / 2;
 
 spider.style.left += `${leftPos}px`;
 spider.style.top += `${topPos}px`;
